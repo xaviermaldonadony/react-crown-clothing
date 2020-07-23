c446e5b805069119d4d2fe244941af45613a5a5d
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-22 23:38:27 -0400

    Added react-router-dom - updated menu ite to navigate withRouter HOC

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = (props) => (
 	<div className='homepage'>
 		<Directory />
 	</div>
