f0be3ebe1b83090acd811a37bfc58605f4c556c5
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-10-19 20:06:35 -0400

    updated .gitignore

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+	apiKey: 'AIzaSyC_x4HMx_cmz6N8ObkDFgIP4pPN-CKJif0',
+	authDomain: 'crown-db-eccd1.firebaseapp.com',
+	databaseURL: 'https://crown-db-eccd1.firebaseio.com',
+	projectId: 'crown-db-eccd1',
+	storageBucket: 'crown-db-eccd1.appspot.com',
+	messagingSenderId: '794846899958',
+	appId: '1:794846899958:web:787ff74fd4d3e7f37dc6d7',
+	measurementId: 'G-E48403QKTW',
+};
+export default FIREBASE_CONFIG;
