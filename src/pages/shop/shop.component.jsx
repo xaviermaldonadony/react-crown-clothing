b4c1db9bf203dc1f6b682992e3ca4b6bf59a3522
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-11-04 04:58:47 -0500

    Moved fetchCollections to a redux hadle async event with redux-thunk - updated store, shop actions, types, selectors, reeucer - implemented hoc spinner that runs while fetching shop collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
-import {
-	firestore,
-	convertCollectionsSnapShotToMap,
-} from '../../firebase/firebase.utils';
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
 
-import { updateCollections } from '../../redux/shop/shop.actions';
-
-import WithSpinner from '../../components/with-spinner/with-spinnger.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import CollectionsOverView from '../../components/collections-overview/collections-overview.components';
 import CollectionPage from '../collection/collection.component';
@@ -19,39 +16,14 @@ const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 // we get match becuase or component is inside of a Route
 class ShopPage extends React.Component {
-	state = {
-		loading: true,
-	};
-	unsubscribeFromSnapshot = null;
-
 	componentDidMount() {
-		const { updateCollections } = this.props;
-		const collectionRef = firestore.collection('collections');
-
-		// fetch(
-		// 	'https://firestore.googleapis.com/v1/projects/crown-db-eccd1/databases/(default)/documents/collections'
-		// )
-		// 	.then((response) => response.json())
-		// 	.then((collections) => console.log(collections));
-
-		// promise, no observer pattern
-		// we will only get new data when we remount
-		collectionRef.get().then((snapshot) => {
-			const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
-			updateCollections(collectionsMap);
-			this.setState({ loading: false });
-		});
-
-		// collectionRef.onSnapshot(async (snapshot) => {
-		// 	const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
-		// 	updateCollections(collectionsMap);
-		// 	this.setState({ loading: false });
-		// });
+		const { fetchCollectionsStartAsync } = this.props;
+		// call the action the moment component mounts
+		fetchCollectionsStartAsync();
 	}
 
 	render() {
-		const { match } = this.props;
-		const { loading } = this.state;
+		const { match, isCollectionFetching } = this.props;
 
 		return (
 			<div className='shop-page'>
@@ -59,15 +31,21 @@ class ShopPage extends React.Component {
 					exact
 					path={`${match.path}`}
 					render={(props) => (
-						<CollectionsOverViewWithSpinner isLoading={loading} {...props} />
+						<CollectionsOverViewWithSpinner
+							isLoading={isCollectionFetching}
+							{...props}
+						/>
 					)}
 				/>
 				<Route
 					path={`${match.path}/:collectionId`}
 					// render takes afunction where the params in the func, are the parms the omponent will
 					// receive
-					irender={(props) => (
-						<CollectionPageWithSpinner isLoading={loading} {...props} />
+					render={(props) => (
+						<CollectionPageWithSpinner
+							isLoading={isCollectionFetching}
+							{...props}
+						/>
 					)}
 				/>
 			</div>
@@ -75,9 +53,12 @@ class ShopPage extends React.Component {
 	}
 }
 
-const mapDispatchToProps = (dispatch) => ({
-	updateCollections: (collectionsMap) =>
-		dispatch(updateCollections(collectionsMap)),
+const mapStateToProps = createStructuredSelector({
+	isCollectionFetching: selectIsCollectionFetching,
 });
 
-export default connect(null, mapDispatchToProps)(ShopPage);
+const mapDispatchToProps = (dispatch) => ({
+	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,43 @@
-import { UPDATE_COLLECTIONS } from './shop.types';
+import {
+	FETCH_COLLECTIONS_FAILURE,
+	FETCH_COLLECTIONS_START,
+	FETCH_COLLECTIONS_SUCCESS,
+} from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-	type: UPDATE_COLLECTIONS,
+import {
+	firestore,
+	convertCollectionsSnapShotToMap,
+} from '../../firebase/firebase.utils';
+
+// we usually write a function that returns an action/object
+// we are going to write a function that returns a function
+// that gets dispatch in it, when ever dispatch is called it fill fire multiple actions
+export const fetchCollectionsStart = () => ({
+	type: FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+	type: FETCH_COLLECTIONS_SUCCESS,
 	payload: collectionsMap,
 });
+
+export const fetchCollectionsFailure = (error) => ({
+	type: FETCH_COLLECTIONS_FAILURE,
+	payload: error,
+});
+
+// Thunk ex, is just a function that returns a function that gets acces to dispatch
+export const fetchCollectionsStartAsync = () => (dispatch) => {
+	const collectionRef = firestore.collection('collections');
+	// dispatches fetchCollectionsStart, which changes our isFetching state to true
+	dispatch(fetchCollectionsStart());
+
+	// it begins this async request this is why we use redux-thunk
+	collectionRef
+		.get()
+		.then((snapshot) => {
+			const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
+			dispatch(fetchCollectionsSuccess(collectionsMap));
+		})
+		.catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+};
