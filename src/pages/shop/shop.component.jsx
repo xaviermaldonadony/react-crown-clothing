9023cb3be983231d09691a25e6e34f80b5d584be
Xavi <xaviermaldonadony@hotmail.com>
2020-11-16 02:48:27 -0500

    updated our appplication with hooks - sign-in component and sign-up component - shop component uses useEffect

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,30 +7,25 @@ import CollectionPageContainer from '../collection/collection.container';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 
 // we get match becuase or component is inside of a Route
-class ShopPage extends React.Component {
-	componentDidMount() {
-		const { fetchCollectionsStart } = this.props;
-		// call the action the moment component mounts
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+	useEffect(() => {
 		fetchCollectionsStart();
-	}
+	}, [fetchCollectionsStart]);
 
-	render() {
-		const { match } = this.props;
-		return (
-			<div className='shop-page'>
-				<Route
-					exact
-					path={`${match.path}`}
-					component={CollectionsOverviewContainer}
-				/>
-				<Route
-					path={`${match.path}/:collectionId`}
-					component={CollectionPageContainer}
-				/>
-			</div>
-		);
-	}
-}
+	return (
+		<div className='shop-page'>
+			<Route
+				exact
+				path={`${match.path}`}
+				component={CollectionsOverviewContainer}
+			/>
+			<Route
+				path={`${match.path}/:collectionId`}
+				component={CollectionPageContainer}
+			/>
+		</div>
+	);
+};
 
 const mapDispatchToProps = (dispatch) => ({
 	fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
